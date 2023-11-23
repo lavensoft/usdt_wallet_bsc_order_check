276e81da61cb3fdd0ba65acdfead8d3e33468a8b
Lavensoft <46833976+lavensoft@users.noreply.github.com>
2023-11-23 14:37:41 +0700

    U

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: '*',
+    allowedHeaders: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
